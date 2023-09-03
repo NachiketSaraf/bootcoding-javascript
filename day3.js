7c3ebe37bf35ec40f18f8f102534b241f943ec6a
NachiketSaraf <sarafnachiket@gmail.com>
2023-09-03 23:10:28 +0530

    day2

---
+++ day3.js
@@ -0,0 +1,3 @@
+let hello = document.getElementById('hello').innerHTML = "hello id"
+let hello2 = document.getElementsByClassName('HELLO2')[0].innerHTML = "Hello class"
+document.querySelector('#hello').innerHTML =  "Hello JS"
